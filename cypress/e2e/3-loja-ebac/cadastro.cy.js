0436801e5a126b410c4b431b4c1c3be80bd9d81a
ivaneidepmn <ivapmn@gmail.com>
2024-03-22 23:32:02 -0300

    adcionando arquivo de  massa de dasdos

--- cypress/e2e/3-loja-ebac/cadastro.cy.js
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -2,7 +2,7 @@
 import { faker } from '@faker-js/faker';
 describe('Funcionalidade: Cadastro', () => {
         beforeEach(() => {
-            cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+            cy.visit('minha-conta')
         });
         it('Deve completar o cadastro com sucesso', () => {
             cy.get('#reg_email').type(faker.internet.email());
--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,13 +1,14 @@
 /// <reference types="cypress" />
+     const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade:login', () => {
     beforeEach(( ) => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
     afterEach(() => {
         cy.screenshot()
   });
-    it('deve fazer login', () => {
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('testfot@ebac.com.br');
         cy.get('#password').type('Self@145!=');
         cy.get('.woocommerce-form > .button').click();
@@ -27,4 +28,18 @@ describe('Funcionalidade:login', () => {
         cy.get('.woocommerce-form > .button').click();
         cy.get('.woocommerce-error > li').should('contain', 'A senha fornecida para o e-mail testfot@ebac.com.br está incorreta.')
 });
-})
+it('Deve fazer login com sucesso - usando massa de dados', () => {
+    cy.get('#username').type(perfil.usuario)
+    cy.get('#password').type(perfil.senha)
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, testfot (não é testfot? Sair)')
+});
+it.only('Deve fazer login com sucesso - usando fixtures', () => {
+    cy.fixture( 'perfil').then(dados => {
+        cy.get('#username').type(dados.usuario, {log: false})
+        cy.get('#password').type(dados.senha , {log:false})
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, testfot (não é testfot? Sair)')
+    })
+});
+});
