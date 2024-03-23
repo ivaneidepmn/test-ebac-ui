3f9bf6eb31e283326681cc468eefb660f2b52b82
ivaneidepmn <ivapmn@gmail.com>
2024-03-22 21:48:20 -0300

    teste de produto da lista

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade: Produtos', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block').first().click()
+
+        cy.get('#tab-title-additional_information > a').should('contain','Informação adicional')
+
+        });
+    it('Deve selecionar o terceiro nome da lista', () => {
+    cy.get('.block-inner').eq(2).click();
+    cy.get('#tab-title-description > a').should('contain','Descrição');
+    });
+    it('Deve selecionar produto da lista pelo nome', () => {
+        cy.get('.product-block').contains('Arcadio Gym Short');
+
+    });
+    });
